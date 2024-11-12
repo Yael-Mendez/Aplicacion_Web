4d26835ace7c6c0d6e1dfc61f7c5bdc4ef9e2699
Yael-Mendez <yaelcarlos20@gmail.com>
2024-11-12 13:33:26 -0600

    modulos

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-import streamlit as st
-from gtts import gTTS
-#import os
-
-def create_audiobook(text,language,otuput_file,accent):
-    tts=gTTS(text=text,lang=language,tld=accent)#Almacena con el nombre que se le dio
-    tts.save(otuput_file)
-
-#Titulo de la Aplicacion
+import streamlit as st #importar todas las herramientas necesaria
+from gtts import gTTS #importar las herramientas de voz de google
+def create_audiobook(text,language,output_file,): #generar apartir del texto un audio 
+    tts=gTTS(text= text, lang=language, tld='com') #crea un objeto para obtener una voz a la cual se le asigna un texto el idioma y acento
+    tts.save(output_file) #almacena un audio con el nombre que se le asigne
+    #Título de la aplicación
 st.title("Text2Audio")
-
-#Imput del Texto
 text=st.text_area("Introduce el texto aqui:")
+# Lista desplegable para el Idioma
+languages = {
+    "Español (es)": "es",
+    "Ingles (en)": "en",
+    "Francés (fr)": "fr",
+    "Alemán (de)":"de" 
+}
+language = st.selectbox("selecciona el idioma:", list(languages.keys()))
 
-#Lista Desplegable para el Idioma
-languages={"Español (es)": "es", "Ingles (en)": "en", "Frances (fr)": "fr", "Aleman (de)": "de"}
-language=st.selectbox("Selecciona el idioma:", list(languages.keys()))
-
-#Lista Desplegable para el Acento
-accents={"com": "Estados Unidos", "co.uk": "Reino Unido", "ca": "Canada", "com.au": "Australia", "co.in": "India"}
-accent = st.selectbox("Selecciona el acento:", list(accents.keys()))
-
-#Boton para generar tetx audio
-if st.button("Genera Audio"):
+# Lista desplegable para el acento
+accent = {
+    "com": "Estados Unidos",
+    "co.uk": "Reino Unido",
+    "ca": "Canadá",
+    "com.au": "Australia",
+    "co.in":"India"
+}
+accent = st.selectbox("Selecciona el acento:", list(accent.keys()))
+# Boton para Generar Audiolibro
+if st.button("Generar Audiolibro"):
     if text:
-        output_file="audiolibro.mp3"
-        create_audiobook(text, languages[language], output_file, accent)
-        st.audio(output_file,format="audio/mp3")
-    else:
-        st.error("Por favor, introduce un texto.")
+        output_file = "audiolibro.mp3"
+        create_audiobook(text, languages[language], output_file)
+        st.audio(output_file, format="audio/mp3")
+else:
+    st.error("Por favor, introduce un texto.")
